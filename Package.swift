b125fcbb4f58ff865d3ff61281fda921af651bd6
aroussinda <aroussinda55@gmail.com>
2025-02-25 10:02:59 +0100

    packageswift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+//
+//  Package.swift
+//  MySDK
+//
+//  Created by Sinda Arous on 25/02/2025.
+//
+
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "MySDK",
+    platforms: [
+        .iOS(.v13)
+    ],
+       products: [
+           .library(name: "MySDK", targets: ["MySDK"])
+       ],
+       targets: [
+           .target(name: "MySDK", path: "Sources")
+       ]
+)
+
