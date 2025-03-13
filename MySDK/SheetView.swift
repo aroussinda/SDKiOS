2083e8d850815f675c7af7a51d0f28af44ab293f
aroussinda <aroussinda55@gmail.com>
2025-03-13 13:10:15 +0100

    text

--- MySDK/SheetView.swift
+++ MySDK/SheetView.swift
@@ -38,7 +38,7 @@ struct SheetViewChallenge: View {
     
     private let footerCustomization = STDSFooterCustomization.defaultSettings()
     private let imageLoader = STDSImageLoader()
-    
+    private let navigationCustomization = STDSNavigationBarCustomization.defaultSettings()
     var response: STDSChallengeResponse
     var onClose: () -> Void
   
@@ -48,6 +48,16 @@ struct SheetViewChallenge: View {
             //case .html:
         case .multiSelect:
             MultiSelectChallengeView( response: response )
+                .navigationBarTitle(navigationCustomization.headerText,displayMode: .inline)
+                .navigationBarItems(trailing: Button(action: {
+                    // Your action here
+                }) {
+                    Text(navigationCustomization.buttonText) // Wrap the string in Text
+                })
+
+               
+            //.navigationBarItems(trailing: Button(navigationCustomization.buttonText))
+              
         case .oob:
             oobChallengeView( response: response )
         case .singleSelect:
